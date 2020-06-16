b471808b8b7828f23bed25be9567bc5b718611f3
adrianolmedo <adrianolmedo.ve@gmail.com>
2020-06-15 20:15:31 -0400

    New features

--- my_plugin/controller/admin/settings.php
+++ my_plugin/controller/admin/settings.php
@@ -13,13 +13,13 @@ class CAdminMyPluginSettings extends AdminSecBaseModel
                 if (!$field_three) $field_three = 0;
 
                 if (!is_numeric($field_three)) {
-                    osc_add_flash_error_message(__("Enter a valid number.", 'my_plugin'), 'admin');
+                    osc_add_flash_error_message(__("Enter a valid number.", MY_PLUGIN_PREF), 'admin');
                 } else {
-                    osc_set_preference('field_one', Params::getParam('field_one'), 'my_plugin', 'BOOLEAN');
-                    osc_set_preference('field_two', Params::getParam('field_two'), 'my_plugin', 'BOOLEAN');
-                    osc_set_preference('field_three', $field_three, 'my_plugin', 'STRING');
+                    osc_set_preference('field_one', Params::getParam('field_one'), MY_PLUGIN_PREF, 'BOOLEAN');
+                    osc_set_preference('field_two', Params::getParam('field_two'), MY_PLUGIN_PREF, 'BOOLEAN');
+                    osc_set_preference('field_three', $field_three, MY_PLUGIN_PREF, 'STRING');
 
-                    osc_add_flash_ok_message(__("The plugin is now configured.", 'my_plugin'), 'admin');
+                    osc_add_flash_ok_message(__("The plugin is now configured.", MY_PLUGIN_PREF), 'admin');
                 }
                 ob_get_clean();
                 $this->redirectTo(osc_route_admin_url('my-plugin-admin-settings'));
